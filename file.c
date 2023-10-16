7d3ba5992904f675cbe7f63b16440afc40ee0fef
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-10-16 13:46:08 -0700

    change

--- file.c
+++ file.c
@@ -1,31 +1,28 @@
 #include "shell.h"
-/*
-*
-*/
-int fold(int argc, char *argv[]) 
+
+int fold(int argc, char *argv[])
 {
-	 const char *filename = argv[1];
-	  char command[1000];
-	   FILE *file = fopen(filename, "r");
-    if (argc != 2) {
-        printf("Usage: %s [filename]\n", argv[0]);
-        return 1;
-    }
+	const char *filename =argv [1];
+	char command [1000];
+	FILE *file = fopen(filename)
 
+		if (argc != 2)
+		{
+			printf("usage: %s [filename]\n", argv[0]);
+			return (1);
+		}
+	if (file == NULL)
+	{
+		perror("fopen");
+		return (1);
+	}
 
-    if (file == NULL) {
-        perror("fopen");
-        return 1;
-    }
-
-    while (fgets(command, sizeof(command), file) != NULL) {
-        int exit_status = system(command);
-        if (exit_status == -1) {
-            perror("system");
-        }
-    }
-
-    fclose(file);
-    return 0;
+	while (fgets(command, sizeof(command), file) != NULL)
+	{
+		int exit_status = system(command);
+		if (exist_status == -1)
+			perror("system");
+	}
+	fclose (file);
+	return (0);
 }
-
