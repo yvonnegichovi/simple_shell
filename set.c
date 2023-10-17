20cf546e2cb36f1aef904b8fc3c12542fb4d5e39
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-10-17 06:37:22 -0700

    betty

--- set.c
+++ set.c
@@ -1,33 +1,38 @@
 #include "shell.h"
-/*
- *
- */
+/**
+* allows you to use the setenv and unsetenv commands. 
+* You can compile and run this C program to test the functionality.
+*  To exit the shell, simply enter the "exit" command.
+*/
+int my_setenv(const char *name, const char *value)
+{
+	if (name == NULL || value == NULL)
+	{
+		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+		return (-1);
+	}
 
-
-int my_setenv(const char *name, const char *value) {
-    if (name == NULL || value == NULL) {
-        fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-        return -1;
-    }
-
-    if (setenv(name, value, 1) != 0) {
-        perror("setenv");
-        return -1;
-    }
-
-    return 0;
+	if (setenv(name, value, 1) !=0)
+	{
+		perror("setenv");
+		return (-1);
+	}
+	return (0);
 }
 
-int my_unsetenv(const char *name) {
-    if (name == NULL) {
-        fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-        return -1;
-    }
 
-    if (unsetenv(name) != 0) {
-        perror("unsetenv");
-        return -1;
-    }
+int my_unsetenv(const char *name)
+{
+	if (name == NULL)
+	{
+		fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+		return (-1);
+	}
 
-    return 0;
+	if (unsetenv(name) !=0)
+	{
+		perror("unsetenv");
+		return (-1);
+	}
+	return (0);
 }
